7fc7cc6f1b9a2b2231a984add0de21ad24c4a460
Emil Matei <emilmatei97@gmail.com>
2020-04-14 00:14:14 +0300

    Add automation properties to SystemState Table

---
+++ API/Smart garden/Migrations/20200413144911_AddAutomationModeToSystemState.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Smart_garden.Migrations
+{
+    public partial class AddAutomationModeToSystemState : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+         
+            migrationBuilder.AddColumn<bool>(
+                name: "AutomationMode",
+                table: "SystemState",
+                nullable: false,
+                defaultValue: false);
+
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+
+            migrationBuilder.DropColumn(
+                name: "AutomationMode",
+                table: "SystemState");
+
+            migrationBuilder.AddColumn<int>(
+                name: "BoardKey",
+                table: "IrigationSystem",
+                nullable: false,
+                defaultValue: 0);
+
+        }
+    }
+}
